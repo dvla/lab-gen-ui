0749a77f7e45e17ef52fe66861ad9a4f1c74fc11
Daniel Williams <danwill89@hotmail.com>
2023-11-21 10:02:26 +0000

    GovUK added, Health route added

---
+++ src/app/diagrams/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { Footer, H1, Page } from 'govuk-react';
+
+export default function Diagrams() {
+    return (
+        <>
+            <Page>
+                <H1>Diagrams</H1>
+            </Page>
+            <Footer />
+        </>
+    );
+}
---
+++ src/app/jira-tickets/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { Footer, H1, Page } from 'govuk-react';
+
+export default function JiraTickets() {
+    return (
+        <>
+            <Page>
+                <H1>Jira Tickets</H1>
+            </Page>
+            <Footer />
+        </>
+    );
+}
