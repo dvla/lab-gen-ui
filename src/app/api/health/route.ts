14ed1acb5f30d60baf177ab5c3dd6eec3d54bc17
Daniel Williams <danwill89@hotmail.com>
2023-11-21 11:52:20 +0000

    Update route.ts

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
     return NextResponse.json({  "status": "UP",  "description": new Date().toISOString() }, { status: 200 });
