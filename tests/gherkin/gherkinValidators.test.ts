2c421340635b8abd1b10f64ec2e58777fa546fda
Jamie Quinn <jamie.quinn@dvla.gov.uk>
2024-04-25 08:51:08 +0000

    ET-1506: fix Gherkin fullValidation test suite
    fix: ET-1506: fix Gherkin fullValidation test suite

--- tests/gherkin/gherkinValidators.test.ts
+++ tests/gherkin/gherkinValidators.test.ts
@@ -388,131 +388,135 @@ describe('Gherkin Validators', () => {
                 },
                 comments: [],
             };
-            expect(fullValidation(doc)).toBe(true);
+            const result = fullValidation(doc);
+            expect(result.valid).toBe(true);
         });
 
         it('should return false when scenario title validation fails', () => {
-          const doc: GherkinDocument = {
-            feature: {
-                location: { line: 1, column: 1 },
-                tags: [],
-                language: '',
-                keyword: '',
-                description: '',
-                name: 'My Feature',
-                children: [
-                    {
-                        scenario: {
-                            location: { line: 1, column: 1 },
-                            name: 'Scenario 1',
-                            steps: [],
-                            tags: [],
-                            keyword: 'Scenario',
-                            description: '',
-                            id: '1',
-                            examples: [],
+            const doc: GherkinDocument = {
+                feature: {
+                    location: { line: 1, column: 1 },
+                    tags: [],
+                    language: '',
+                    keyword: '',
+                    description: '',
+                    name: 'My Feature',
+                    children: [
+                        {
+                            scenario: {
+                                location: { line: 1, column: 1 },
+                                name: 'Scenario 1',
+                                steps: [],
+                                tags: [],
+                                keyword: 'Scenario',
+                                description: '',
+                                id: '1',
+                                examples: [],
+                            },
                         },
-                    },
-                    {
-                        scenario: {
-                            location: { line: 1, column: 1 },
-                            name: 'Scenario 1',
-                            steps: [],
-                            tags: [],
-                            keyword: 'Scenario',
-                            description: '',
-                            id: '1',
-                            examples: [],
+                        {
+                            scenario: {
+                                location: { line: 1, column: 1 },
+                                name: 'Scenario 1',
+                                steps: [],
+                                tags: [],
+                                keyword: 'Scenario',
+                                description: '',
+                                id: '1',
+                                examples: [],
+                            },
                         },
-                    },
-                ],
-            },
-            comments: [],
-        };
-            expect(fullValidation(doc)).toBe(false);
+                    ],
+                },
+                comments: [],
+            };
+            const result = fullValidation(doc);
+            expect(result.titles && result.valid).toBe(false);
         });
 
         it('should return false when step validation fails', () => {
-          const doc: GherkinDocument = {
-            feature: {
-                location: { line: 1, column: 1 },
-                tags: [],
-                language: '',
-                keyword: '',
-                description: '',
-                name: 'My Feature',
-                children: [
-                    {
-                        scenario: {
-                            location: { line: 1, column: 1 },
-                            name: 'Scenario 1',
-                            steps: [],
-                            tags: [],
-                            keyword: 'Scenario',
-                            description: '',
-                            id: '1',
-                            examples: [],
+            const doc: GherkinDocument = {
+                feature: {
+                    location: { line: 1, column: 1 },
+                    tags: [],
+                    language: '',
+                    keyword: '',
+                    description: '',
+                    name: 'My Feature',
+                    children: [
+                        {
+                            scenario: {
+                                location: { line: 1, column: 1 },
+                                name: 'Scenario 1',
+                                steps: [],
+                                tags: [],
+                                keyword: 'Scenario',
+                                description: '',
+                                id: '1',
+                                examples: [],
+                            },
                         },
-                    },
-                ],
-            },
-            comments: [],
-        };
-            expect(fullValidation(doc)).toBe(false);
+                    ],
+                },
+                comments: [],
+            };
+            const result = fullValidation(doc);
+            expect(result.steps && result.valid).toBe(false);
         });
 
         it('should return false when keyword validation fails', () => {
-          const doc: GherkinDocument = {
-            feature: {
-                location: { line: 1, column: 1 },
-                tags: [],
-                language: '',
-                keyword: '',
-                description: '',
-                name: 'My Feature',
-                children: [
-                    {
-                        scenario: {
-                            location: { line: 1, column: 1 },
-                            name: 'Scenario 1',
-                            steps: [
-                                {
-                                    id: '0',
-                                    keyword: 'INVALID',
-                                    text: 'I have something to eat',
-                                    location: { line: 1, column: 1 },
-                                },
-                                {
-                                    id: '1',
-                                    keyword: 'When',
-                                    text: "I'm hungry",
-                                    location: { line: 1, column: 1 },
-                                },
-                                {
-                                    id: '2',
-                                    keyword: 'And',
-                                    text: "I've been out all day",
-                                    location: { line: 1, column: 1 },
-                                },
-                                {
-                                    id: '3',
-                                    keyword: 'Then',
-                                    text: 'I should feel better',
-                                    location: { line: 1, column: 1 },
-                                },
-                            ],
-                            tags: [],
-                            keyword: 'Scenario',
-                            description: '',
-                            id: '1',
-                            examples: [],
+            const doc: GherkinDocument = {
+                feature: {
+                    location: { line: 1, column: 1 },
+                    tags: [],
+                    language: '',
+                    keyword: '',
+                    description: '',
+                    name: 'My Feature',
+                    children: [
+                        {
+                            scenario: {
+                                location: { line: 1, column: 1 },
+                                name: 'Scenario 1',
+                                steps: [
+                                    {
+                                        id: '0',
+                                        keyword: 'INVALID',
+                                        text: 'I have something to eat',
+                                        location: { line: 1, column: 1 },
+                                    },
+                                    {
+                                        id: '1',
+                                        keyword: 'When',
+                                        text: "I'm hungry",
+                                        location: { line: 1, column: 1 },
+                                    },
+                                    {
+                                        id: '2',
+                                        keyword: 'And',
+                                        text: "I've been out all day",
+                                        location: { line: 1, column: 1 },
+                                    },
+                                    {
+                                        id: '3',
+                                        keyword: 'Then',
+                                        text: 'I should feel better',
+                                        location: { line: 1, column: 1 },
+                                    },
+                                ],
+                                tags: [],
+                                keyword: 'Scenario',
+                                description: '',
+                                id: '1',
+                                examples: [],
+                            },
                         },
-                    },
-                ],
-            },
-            comments: [],
-        };
-            expect(fullValidation(doc)).toBe(false);
+                    ],
+                },
+                comments: [],
+            };
+            const result = fullValidation(doc);
+            expect(result.keywords && result.valid).toBe(false);
         });
     });
 });
